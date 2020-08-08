39d5ab0981a0dee00b5628830b61d761ab50457f
Amar Maharjan <amar@bottle.com.np>
2020-08-08 09:36:11 +0545

    receiver issue fixed

--- android-en/src/ua/gram/munhauzen/AlertReceiver.java
+++ android-en/src/ua/gram/munhauzen/AlertReceiver.java
@@ -4,11 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import ua.gram.munhauzen.utils.Log;
+
 import static com.facebook.FacebookSdk.getApplicationContext;
 
 public class AlertReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.i("AlertReceiver"," triggered");
         NotificationHelper.displayNotification(getApplicationContext(), "title","body");
     }
 }
